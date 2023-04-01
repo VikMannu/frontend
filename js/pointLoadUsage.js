423677859e55378d34524e7c84efe2b42c9e2737
VikMannu <victor.ayala2a@gmail.com>
2023-04-01 10:04:09 -0400

    Casi terminado la carga de uso de puntos

--- js/pointLoadUsage.js
+++ js/pointLoadUsage.js
@@ -1,8 +1,74 @@
-function getAllClients() {
-    fetch('http://127.0.0.1:8080/prueba/clientes')
-        .then(response => response.json())
+const fondoPopup = document.querySelector('#fondo-popup');
+const botonCerrar = document.querySelector('#cerrar-popup');
+
+function cerrarPopup() {
+    fondoPopup.style.display = 'none';
+}
+botonCerrar.addEventListener('click', cerrarPopup)
+
+const loadButton = document.querySelector('#load')
+loadButton.addEventListener('click', function () {
+    const selectedClient = document.querySelector('input[name="client"]:checked')
+    const selectedPointManagement = document.querySelector('input[name="point-management"]:checked')
+    if (selectedClient && selectedPointManagement) {
+        const idClient = selectedClient.value;
+        const idPointManagement = selectedPointManagement.value;
+        pointLoadUsage(idClient, idPointManagement)
+    } else {
+        console.log('Debes seleccionar un cliente');
+    }
+});
+
+function pointLoadUsage(idClient, idPointManagement) {
+    const titulo = document.querySelector('.capa-popup h2');
+    const parrafo = document.querySelector('.capa-popup p');
+    const data = {
+        "idCliente": parseInt(idClient),
+        "idConcepto": parseInt(idPointManagement)
+    }
+    console.log(data)
+    const options = {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+    fetch('http://127.0.0.1:8080/prueba/servicio/uso-puntos', options)
+        .then(response => {
+            console.log(response)
+            if (!response.ok) {
+                throw new Error('Ocurrió un error al hacer la solicitud');
+            }
+            return response.json();
+        })
         .then(data => {
             console.log(data)
+            titulo.innerText = "Exito"
+            parrafo.innerText = data.message
+            fondoPopup.style.display = 'block';
+        })
+        .catch(error => {
+            console.error(error)
+            titulo.innerText = "Error"
+            parrafo.innerText = error.message
+            fondoPopup.style.display = 'block';
+        })
+}
+
+async function getAllClients() {
+    fetch('http://127.0.0.1:8080/prueba/clientes')
+        .then(response => {
+            console.log(response)
+            if (!response.ok) {
+                throw new Error('Ocurrió un error al hacer la solicitud');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data)
+            // Array de promesas
+            const promises = [];
             const clientesTableBody = document.querySelector('#clientes tbody')
             data.forEach(client => {
                 const tr = document.createElement('tr');
@@ -33,6 +99,11 @@ function getAllClients() {
                 emailTd.textContent = client.email
                 tr.appendChild(emailTd)
 
+                const points = getPointsClient(client.id)
+                const pointsTd = document.createElement('td')
+                pointsTd.textContent = points
+                tr.appendChild(pointsTd)
+
                 clientesTableBody.appendChild(tr)
             });
         })
@@ -41,9 +112,34 @@ function getAllClients() {
         })
 }
 
+function getPointsClient(idClient) {
+    fetch(`http://127.0.0.1:8080/prueba/clientes/puntos-cliente/${idClient}`)
+        .then(response => {
+            console.log(response)
+            if (!response.ok) {
+                throw new Error('Ocurrió un error al hacer la solicitud');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data)
+            return data.puntos
+        })
+        .catch(error => {
+            console.error(error)
+            return "0"
+        })
+}
+
 function getAllPointsManagement() {
     fetch('http://127.0.0.1:8080/prueba/administracion_puntos')
-        .then(response => response.json())
+        .then(response => {
+            console.log(response)
+            if (!response.ok) {
+                throw new Error('Ocurrió un error al hacer la solicitud');
+            }
+            return response.json();
+        })
         .then(data => {
             console.log(data)
             const clientesTableBody = document.querySelector('#point-usage-concept tbody')
@@ -54,7 +150,7 @@ function getAllPointsManagement() {
                 radioButtonTd.style.textAlign = 'center'
                 const radioButton = document.createElement('input')
                 radioButton.setAttribute('type', 'radio')
-                radioButton.setAttribute('name', 'point')
+                radioButton.setAttribute('name', 'point-management')
                 radioButton.setAttribute('value', element.id)
                 radioButton.classList.add('centrado')
                 radioButtonTd.appendChild(radioButton)
